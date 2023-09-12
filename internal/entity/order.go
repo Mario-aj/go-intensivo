da37df675829ad9ef70d7e7aec52b7e869da550b
mario-aj <mariojorge1997@mail.ru>
2023-09-12 09:36:25 +0100

    implementing Validation

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	price      float64
@@ -7,12 +9,36 @@ type Order struct {
 	finalPrice float64
 }
 
-func NewOrder(id string, price float64, tax float64) *Order {
-	return &Order{
+func NewOrder(id string, price float64, tax float64) (*Order, error) {
+	order := &Order{
 		ID:    id,
 		price: price,
 		tax:   tax,
 	}
+
+	err := order.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return order, nil
+}
+
+func (order *Order) Validate() error {
+	if order.ID == "" {
+		return errors.New("id is required")
+	}
+
+	if order.price <= 0 {
+		return errors.New("price must be grater than zero")
+	}
+
+	if order.tax < 0 {
+		return errors.New("invalid tax")
+	}
+
+	return nil
 }
 
 func (order *Order) CalculateFinalPrice() {
